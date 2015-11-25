b8f473496a7f0253a4fda2e4d6f3fb0d0a9e3e4b
Robert Jenkins <jinx@starbuck.local>
2015-11-25 10:34:01 -0800

    - added js/app.js - added uplaod buttn to index.html

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var accessKeyId     = process.env.EIVU_AWS_ACCESS_KEY_ID;
+var secretAccessKey = process.env.EIVU_AWS_SECRET_ACCESS_KEY;
+
+var AWS = require('aws-sdk'),
+    fs = require('fs');
+var pathToFile = '/Users/jinx/Dropbox/eBooks/Electron/electron-quick-start-master.zip'
+var bucketName = 'eivutest';
+
+// For dev purposes only
+AWS.config.update({ accessKeyId: accessKeyId, secretAccessKey: secretAccessKey });
+
+// Read in the file, convert it to base64, store to S3
+var fileStream = fs.createReadStream(pathToFile);
+fileStream.on('error', function (err) {
+  if (err) { throw err; }
+});
+
+fileStream.on('open', function () {
+  var s3 = new AWS.S3();
+  s3.putObject({
+    Bucket: bucketName,
+    Key: 'electro-quick-start-master3.zip',
+    Body: fileStream
+  }, function (err) {
+    if (err) { throw err; }
+  });
+});
