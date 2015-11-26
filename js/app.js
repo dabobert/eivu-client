141d4324e01f0d63830e5465fedd17008f3d1ebb
Robert Jenkins <jinx@starbuck.local>
2015-11-26 00:18:23 -0800

    - added a file input to select a folder for upload

--- js/app.js
+++ js/app.js
@@ -4,8 +4,14 @@ var ipc = require('ipc'),
     var requestData = { canvas: "black" };
     // debugger;
     ipc.send('requestForTestFn', requestData);
+  },
+   selectFolder = function(e) {
+    var theFiles = e.target.files;
+    var relativePath = theFiles[0].webkitRelativePath;
+    var folder = relativePath.split("/");
+    debugger;
+    alert(folder[0]);
   };
-
 ipc.on('responseFromTestFn', function(responseArgument){
   console.log(responseArgument.output.data);
   $("#outputWindow").html(responseArgument.output.data);
