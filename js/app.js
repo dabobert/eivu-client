0fb750ba4f0a05d226b7af59ca1b1a819d8aaf5d
Robert Jenkins <jinx@starbuck.local>
2015-11-28 09:30:15 -0800

    - creating homebrew traversal

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ var ipc  = require('ipc'),
     fileHash = {},
     fileData = [];
 
+var fs = require('fs');
+
+
+
   testUpload = function() {
     var badFilenames = ['.DS_Store', '.DS_Store'],
         fullPath     = null,
@@ -14,49 +18,29 @@ var ipc  = require('ipc'),
 
     var requestData = { path: $("#uploadTarget").data("path") };
 
-    filewalker(initialDir)
-      // .on('dir', function(p) {
-      //   console.log('dir:  %s', p);
-      // })
-      .on('file', function(fileLastPath, fileStats) {
-        filename = fileLastPath.split("/").reverse()[0];
-        console.log('raw => ' + filename);
-        fullPath = initialDir + '/' + fileLastPath;
-        if (badFilenames.indexOf(filename) == -1) {
-
-
-md5File(fullPath, function (error, md5) {
-  if (error) {
-    return console.log(error);
-  } else {
-
-    console.log('inside filename => ' + filename);
-    console.log('fullPath => ' + fullPath);
-    console.log('md5 => ' + md5);
-    fileData.push({ fullPath: fullPath, md5: md5, filename: filename, size: fileStats.size })
-    $('<tr id="' + md5 + '"><td>' + filename + '</td><td>' + fileStats.size + '</td><<td>' + md5 + '</td><td>Queued</td></tr>').appendTo('table#fileData tbody');
-    debugger
-  }
-});
 
 
 
 
 
-        }//ends if badfilename
-        // console.log('file: %s, %d bytes', p, s.size);
-      })
-      .on('error', function(err) {
-        console.error(err);
-      })
-      .on('done', function() {
-        console.log(window.fileData);
-      })
-    .walk();
+
 
     ipc.send('requestForTestFn', requestData);
   },
-   assignDataPath = function(e) {
+  traverseFileSystem = function(currentPath) {
+    console.log(currentPath);
+    var files = fs.readdirSync(currentPath);
+    for (var i in files) {
+      var currentFile = currentPath + '/' + files[i];
+      var stats = fs.statSync(currentFile);
+      if (stats.isFile()) {
+        console.log(currentFile);
+      } else if (stats.isDirectory()) {
+        traverseFileSystem(currentFile);
+      }
+    }
+  },
+  assignDataPath = function(e) {
     //due to security reasons the value of a file input can not be set, so we will assign a data-path attribute and the code will use that
     $("#uploadTarget").data("path", e.target.files[0].path)
   };
