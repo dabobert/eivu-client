ad44e49a3818bfd3637d8237d7b1b43edf7cedbb
Robert Jenkins <jinx@starbuck.local>
2015-11-28 23:12:43 -0800

    - renamed variable that had been renamed during testing/debugging

--- js/app.js
+++ js/app.js
@@ -16,10 +16,10 @@ var ipc  = require('ipc'),
     Folder.traverse(initialDir, function(fullPath, fileStats) {
       var md5Promise = new Promise((resolve, reject) => md5File(fullPath, function (error, md5) {
         if (error) return console.log(error)
-        var tester = CloudFile.toFilename(fullPath);
-        console.log(tester)
-        fileData.push({ fullPath: fullPath, md5: md5, filename: tester, size: fileStats.size })
-        $('<tr id="' + md5 + '"><td>' + tester + '</td><td>' + fileStats.size + '</td><<td>' + md5 + '</td><td>Queued</td></tr>').appendTo('table#fileData tbody');
+        var filename = CloudFile.toFilename(fullPath);
+        console.log(filename)
+        fileData.push({ fullPath: fullPath, md5: md5, filename: filename, size: fileStats.size })
+        $('<tr id="' + md5 + '"><td>' + filename + '</td><td>' + fileStats.size + '</td><<td>' + md5 + '</td><td>Queued</td></tr>').appendTo('table#fileData tbody');
       resolve(md5);
       }));//end md5File
       
