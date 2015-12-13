4c038ca0bf5876440c42719e2152906f577da251
Robert Jenkins <bobert@gmail.com>
2015-12-13 00:59:12 -0500

    - by using the package md5-promised i was able to solve the race   condition with the callbacks

--- js/app.js
+++ js/app.js
@@ -2,25 +2,48 @@
 var ipc  = require('ipc'),
     fs   = require('fs'),
     async= require("async"),
+    hash = require('md5-promised'),
      $   = require('jQuery'),
     fileInfo = [],
     md5File  = require('md5-file');
-    // md5Promises = [];
 
 
 var queue = async.queue(function(singleFileInfo, callback) {
   setTimeout(function(){ 
     var data={};
-    md5File(singleFileInfo.fullPath, function (error, md5) {
-      if (error) return console.log(error);
-      var filename = CloudFile.toFilename(singleFileInfo.fullPath);
-      if (Folder.badFilenames().indexOf(filename) != -1) return console.log('skipping ' + filename);
+
+
+
+hash(singleFileInfo.fullPath).then(function(md5) {
+  var filename = CloudFile.toFilename(singleFileInfo.fullPath);
+
 
       $('<tr id="' + md5 + '"><td class="filename">' + filename.substring(0,50) + '</td><td class="size">' + singleFileInfo.fileStats.size + '</td><td class="md5">' + md5 + '</td><td class="status">Queued</td></tr>').appendTo('table#fileData tbody');
       data = { fullPath: singleFileInfo.fullPath, md5: md5, filename: filename, size: singleFileInfo.fileStats.size }
       fileInfo.push( data );
-    });//end md5File
-    callback(null, data);
+callback(null, data);
+
+}).fail(function(err) {
+  console.log('Could not hash', err, err.stack);
+});
+
+
+
+    // md5File(singleFileInfo.fullPath, function (error, md5) {
+
+
+
+
+
+    //   if (error) return console.log(error);
+    //   var filename = CloudFile.toFilename(singleFileInfo.fullPath);
+    //   // if (Folder.badFilenames().indexOf(filename) != -1) return console.log('skipping ' + filename);
+
+    //   $('<tr id="' + md5 + '"><td class="filename">' + filename.substring(0,50) + '</td><td class="size">' + singleFileInfo.fileStats.size + '</td><td class="md5">' + md5 + '</td><td class="status">Queued</td></tr>').appendTo('table#fileData tbody');
+    //   data = { fullPath: singleFileInfo.fullPath, md5: md5, filename: filename, size: singleFileInfo.fileStats.size }
+    //   fileInfo.push( data );
+    // });//end md5File
+    // callback(null, data);
   }, 0); //end setTimeout  
 }, 20); //Only allow 20 copy requests at a time
 
@@ -47,7 +70,7 @@ queue.drain = function() {
           //ie we are failing a race condition
           console.log("in callback")
           if (error) return console.log(error);
-          // console.log(data.filename + " ==> " + data.md5)
+          console.log(data.filename + " ==> " + data.md5)
         });
       } else {
         console.log('Not cloud playable. skipping: ' + fullPath);
