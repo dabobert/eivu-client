2f8a28e9b95d5526eafca604011759a35fe66902
Robert Jenkins <bobert@gmail.com>
2015-12-12 20:36:31 -0500

    - this is actually working

--- js/app.js
+++ js/app.js
@@ -1,10 +1,42 @@
 
 var ipc  = require('ipc'),
     fs   = require('fs'),
+    async= require("async"),
      $   = require('jQuery'),
     fileInfo = [],
     md5File  = require('md5-file'),
     md5Promises = [];
+
+
+
+
+
+
+// var queue = async.queue(createMd5, 10); // Run ten simultaneous uploads
+var queue = async.queue(function(singleFileInfo, callback) {
+// var queue = async.queue(function(fullPath, fileStats, callback) {
+  var data={}, promise = new Promise((resolve, reject) => md5File(singleFileInfo.fullPath, function (error, md5) {
+    if (error) return console.log(error);
+
+    var filename = CloudFile.toFilename(singleFileInfo.fullPath);
+
+    if (Folder.badFilenames().indexOf(filename) != -1) return console.log('skipping ' + filename);
+
+    $('<tr id="' + md5 + '"><td class="filename">' + filename.substring(0,50) + '</td><td class="size">' + singleFileInfo.fileStats.size + '</td><td class="md5">' + md5 + '</td><td class="status">Queued</td></tr>').appendTo('table#fileData tbody');
+    data = { fullPath: singleFileInfo.fullPath, md5: md5, filename: filename, size: singleFileInfo.fileStats.size }
+    fileInfo.push( data );
+    resolve( data );
+  }));//end md5File
+  
+  //store value for promise
+  md5Promises.push(promise);
+  callback(null, data);
+
+
+}, 20); //Only allow 20 copy requests at a time
+
+
+
 
   testUpload = function() {
     // var badFilenames = ['.DS_Store', '.DS_Store'],
@@ -13,21 +45,16 @@ var ipc  = require('ipc'),
 
     Folder.traverse(initialDir, function(fullPath, fileStats) {
       console.log(fullPath)
-      var promise = new Promise((resolve, reject) => md5File(fullPath, function (error, md5) {
+      queue.push({fullPath: fullPath, fileStats: fileStats },function(error, data){
+        console.log("in callback")
+        // debugger;
         if (error) return console.log(error);
+        // $('<tr id="' + data.md5 + '"><td class="filename">' + data.filename.substring(0,50) + '</td><td class="size">' + data.size + '</td><td class="md5">' + md5 + '</td><td class="status">Queued</td></tr>').appendTo('table#fileData tbody');
+        console.log(data.filename + " ==> " + data.md5)
+      });
 
-        var filename = CloudFile.toFilename(fullPath);
 
-        if (Folder.badFilenames().indexOf(filename) != -1) return console.log('skipping ' + filename);
 
-        $('<tr id="' + md5 + '"><td class="filename">' + filename.substring(0,50) + '</td><td class="size">' + fileStats.size + '</td><td class="md5">' + md5 + '</td><td class="status">Queued</td></tr>').appendTo('table#fileData tbody');
-        data = { fullPath: fullPath, md5: md5, filename: filename, size: fileStats.size }
-        fileInfo.push( data );
-        resolve( data );
-      }));//end md5File
-      
-      //store value for promise
-      md5Promises.push(promise);
     });//end traverse
 
     Promise.all(md5Promises).then(function(value) {
