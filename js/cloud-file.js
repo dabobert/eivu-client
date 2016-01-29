0300c5127ebb14fd227bd53f633424fbdee21140
Robert Jenkins <bobert@gmail.com>
2016-01-28 22:41:08 -0500

    - now CloudFile.readMp4Tags returns data in the proper structure

--- js/cloud-file.js
+++ js/cloud-file.js
@@ -82,19 +82,21 @@ class CloudFile {
     })
 
     parser.on('finish', function () {
-      var metadata = {
-        artist: [],
+      var extended_info = {
         duration: stream.info.duration,
-        tags: []
+        title: stream.info["\uFFFDnam"],
+        metadata: {
+          artist: [],
+          tags: []
+        }
       }
 
       var translation_hash = { title: "\uFFFDnam", show: "tvsh", network: "tvnn", episode_id: "tven" };
       var translation_hash_multi = { artist: "\uFFFDART", tags: "desc" };
 
-
       $.each(translation_hash, function( new_key, orig_key ) {
         if (stream.info[orig_key]) {
-          metadata[new_key] = stream.info[orig_key];
+          extended_info.metadata[new_key] = stream.info[orig_key];
         }
       });
 
@@ -102,18 +104,22 @@ class CloudFile {
       $.each(translation_hash_multi, function( new_key, orig_key ) {
         if (stream.info[orig_key]) {
           //removes spaces around commas, and splits result into an array
-          metadata[new_key] = metadata[new_key].concat(stream.info[orig_key].toLowerCase().match( /(?=\S)[^,]+?(?=\s*(,|$))/g ))
+          extended_info.metadata[new_key] = extended_info.metadata[new_key].concat(stream.info[orig_key].toLowerCase().match( /(?=\S)[^,]+?(?=\s*(,|$))/g ))
         }
       });
 
       //add all artists to tags
-      metadata.tags = metadata.tags.concat(metadata.artist)
+      extended_info.metadata.tags = extended_info.metadata.tags.concat(extended_info.metadata.artist)
       //only keep unique values
-      metadata.tags = metadata.tags.filter(function(itm,i,a){
+      extended_info.metadata.tags = extended_info.metadata.tags.filter(function(itm,i,a){
         return i==a.indexOf(itm);
       });
 
-      callback(metadata)
+      //move tags out of metadata. we want both DRY code (ie to use translation_hash_multi) and tags at the root
+      extended_info.tags = extended_info.metadata.tags;
+      delete extended_info.metadata.tags
+
+      callback(extended_info)
     });
   }
 
